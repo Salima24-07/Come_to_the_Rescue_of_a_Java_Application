d8e794ddeb45711319e9e558102930f5c5b9249e
salima <salima.hafirassou@hotmail.com>
2020-11-18 20:13:25 +0100

    mise a jour de la class ReadSymptomDataFromFile avec utilisation des Maps

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -8,10 +8,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 /*
@@ -19,21 +22,25 @@ import java.util.Set;
  * 
  **/
 public class ReadSymptomDataFromFile implements ISymptomReader {
-	
-	// Methode - GetSymptoms() - qui lit un ficher et insert des elements de type "Symptom" dans une Map 	
-	@Override
-	public HashMap<String, Integer> addElementToMap(String path_File) {
 
-		HashMap<String, Integer> map = new HashMap();
+	private String filepath;
+	
+	public ReadSymptomDataFromFile (String filepath) {
+		this.filepath = filepath;
+	}
+	// Methode - GetSymptoms() - qui lit un ficher et insert des elements de type "Symptom" dans une liste 	
+	@Override
+	public Map<String, Integer> addElementToMap(String path_File) {
+		Map<String, Integer> map = new HashMap<String, Integer>(); 
 		BufferedReader bd = null;
 		FileReader file_Read = null;
-		
+		if (filepath != null) {
 			try {
 				/*
-				 * lire le fichier ligne par ligne,
-				  si la valeur de ligne est égale à la clé d'un élement dans la Map, 
-				  alors, on incrémente sa valeur 
-				  sinon, on rajoute un élement qui prend la valeur de la ligne comme clé et on initialise sa valeur à 1.
+				 * lire le fichier ligne par ligne, puis on utilise le filtre suivant: 
+				  si la valeur de ligne est égale à la propieté "symptom" d'un objet element dans la liste, 
+				  alors, on incrémente la valeur de son propiété "occurence"
+				  sinon, on crée un objet pour le nouveau symptom et initialise sa valeur d'occurence à 1 et on l'insert dans la liste.
 				 */ 
 		    	file_Read = new FileReader (path_File);
 				bd = new BufferedReader (file_Read );
@@ -43,45 +50,33 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 					if(map.get(symptom) == null) {
 						map.put(symptom, 1);
 					}else {
-						map.put(symptom, map.get(symptom)+1 );	
+						map.put(symptom, map.get(symptom)+1 );
 					}
-					symptom = bd.readLine();					
+					symptom = bd.readLine();
 				}
-				// Affiche les valeur et les clés des elements du Map
-				Set<String> set = map.keySet();
-				Iterator<String> iterator = set.iterator();
-				while(iterator.hasNext()) {
-					String symptoms = iterator.next().toString();
-					System.out.print(" " + symptoms+" --> " + map.get(symptoms));	
-				}
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			
+		}
+	    	    System.out.println(" ");
 		return map;
 		
 	}
 
 	/*
-	    Methode - writeAllListInFile() - qui prend comme parametre une Map, 
+    	Methode - writeAllListInFile() - qui prend comme parametre une Map, 
 		et ecrit les valeurs et clés de ses elements, 
-		dans le fichier passé en parametre, en utilisant les étérateurs
-	 */
+		dans le fichier passé en parametre
+	*/
 	@Override
-	public void writeAllMapInFile(HashMap<String,Integer> map, String name_File) {
+	public void writeAllMapInFile(Map<String,Integer> symptoms, String path_File) {
 		
-		FileWriter fw;
 		try {
 			
-			fw = new FileWriter(name_File);
-			PrintWriter pw = new PrintWriter(fw);
-			Set<String> set = map.keySet();
-			Iterator<String> iterator = set.iterator();
-			while(iterator.hasNext()) {
-				String symptoms = iterator.next().toString();
-				pw.println(symptoms+" = "+ map.get(symptoms));
-				System.out.print("writer"+symptoms+" --> "+ map.get(symptoms));	
-			}
-			fw.close();			
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path_File)));
+			symptoms.entrySet().forEach(s->pw.println(s.getKey() +" = "+ s.getValue()));
 			pw.close();
 			  
 		} catch (IOException e1) {
