0ba413c17267bf891d757e435cc6ca0ec632d495
salima <salima.hafirassou@hotmail.com>
2020-11-24 23:10:33 +0100

    Gérer les conflits entre les deux versions précedentes

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,25 +1,14 @@
 package com.hemebiotech.analytics;
 
-import java.util.List;
-/**
- * 
- * @author 33641 nassssiiimm
- *
- */
+import java.util.Map;
 
 /** 
  * @author salima
  */
 
 public interface ISymptomReader {
-<<<<<<< HEAD
-	
-	List<Symptom> getSymptoms(String filepath);
-	void writeAllListInFile(List<Symptom> symptoms_OCcurnce, String name_File);
-	
-=======
+
 	Map<String, Integer> addElementToMap( String path_File );
 	Map<String, Integer> sortMap(Map<String, Integer> map);
 	void writeAllMapInFile (Map<String,Integer> map, String path_File);
->>>>>>> finalVersion
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -5,46 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-/**
- * 
- * @author 33641 moihkugjydchyjclo:u!hmouifvktydcjtrx
- *
- */
-public class ReadSymptomDataFromFile implements ISymptomReader {
-	
-	
-	
-	public List<Symptom> getSymptoms(String filepath) {
-		
-		List<Symptom> symptoms = null;
-			try {
-				/*
-				 * lire le fichier ligne par ligne, puis on utilise le filtre suivant: 
-				  si la valeur de ligne est égale à la propieté "symptom" d'un objet element dans la liste, 
-				  alors, on incrémente la valeur de son propiété "occurence"
-				  sinon, on crée un objet pour le nouveau symptom et initialise sa valeur d'occurence à 1 et on l'insert dans la liste.
-				 */ 
-								
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				symptoms = new ArrayList<Symptom>();
-				String line = reader.readLine();
-				
-				while (line != null) {	
-					String l = line;
-					Symptom symptom = symptoms.stream()
-		  					  .filter(s -> l.equals(s.getSymptom()))
-		  		  			  .findAny().orElse(null);
-					if(symptom != null) {
-						symptom.setOccurence((symptom.getOccurence())+1);
-					}else {
-						symptoms.add(new Symptom(line, 1));
-=======
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -95,44 +55,14 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 						symptoms.put(symptom, 1);
 					}else {
 						symptoms.put(symptom, symptoms.get(symptom)+1 );
->>>>>>> finalVersion
 					}
-					line = reader.readLine();
+					symptom = bd.readLine();
 				}
 				
-<<<<<<< HEAD
-				reader.close();
-			} catch (Exception e) {
-=======
-			} catch (IOException e) {
->>>>>>> finalVersion
-				e.printStackTrace();
-			}finally {
-				//todo
-			}
-<<<<<<< HEAD
-			Collections.sort(symptoms, Comparator.comparing(Symptom::getSymptom));
-			symptoms.forEach(s->System.out.println(s.getSymptom()+ " = " +s.getOccurence()));
-		return symptoms;
-	}
-	
-	
-	@Override
-	public void writeAllListInFile(List<Symptom> symptoms, String name_File) {
-			
-			FileWriter fw;
-			try {
-				
-				PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(name_File)));				 
-				symptoms.forEach(s->pw.println(s.getSymptom()+" = "+ s.getOccurence()));
-				pw.close();
-				  
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		}
 
-=======
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 		return sortMap(symptoms);
 		
@@ -153,12 +83,11 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			// Afficher les symptoms et ses occurences dans la console
 			symptoms.entrySet().forEach(s->pw.println(s.getKey() +" = "+ s.getValue()));
 			  
-		} catch (IOException e1) {
+		} catch (Exception e1) {
 			//Afficher les traces des exceptions
 			e1.printStackTrace();
 		}
 	}
 
-	
->>>>>>> finalVersion
+
 }
