a5b39ff011ca031d0077184fe3d81b85b93a9c94
salima <salima.hafirassou@hotmail.com>
2020-11-24 22:58:30 +0100

    java doc main()

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,6 +4,14 @@ import java.util.Map;
 
 public class AnalyticsCounter {
 	
+	/**
+	 * Initialize the paths of the source and result files, 
+	 * and send them by parameter by calling the methods "addElementToMap" and "writeAllMapInFile"
+	   of the class "ReadSymptomDataFromFile" 
+	 * @author salima
+	 * @param args
+	 */
+	
 	public static void main(String args[]) throws Exception {
 		String pathFileSymptoms = "symptoms.txt"; // chemin relatif de fichier qui contient les symptoms 
 		String nameFileResults = "results.out"; // fichier resultat qui contient le nombre d'occurence de chaque symptom
