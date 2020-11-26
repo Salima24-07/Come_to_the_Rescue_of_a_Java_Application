6f8726e34fe81d8666a4a9f5c01c2bcf40069aa8
salima <salima.hafirassou@hotmail.com>
2020-11-26 13:22:49 +0100

    FinalVersion, correction des conflits des commits

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,14 +1,5 @@
 package com.hemebiotech.analytics;
 
-<<<<<<< Updated upstream
-import java.util.List;
-
-public interface ISymptomReader {
-	
-	List<Symptom> GetSymptoms(String filepath);
-	void writeAllListInFile(List<Symptom> symptoms_OCcurnce, String name_File);
-	
-=======
 import java.util.Map;
 
 /** 
@@ -21,5 +12,4 @@ public interface ISymptomReader {
 	Map<String, Integer> addElementToMap( String pathFile );
 	Map<String, Integer> sortMap(Map<String, Integer> map);
 	void writeAllMapInFile (Map<String,Integer> map, String pathFile);
->>>>>>> Stashed changes
 }
